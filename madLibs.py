dc5b9692d979391b179e5b12780b52e46ac0697c
charliedavidhoward <41549599+charliedavidhoward@users.noreply.github.com>
2019-07-01 18:03:10 +0100

    Create madLibs.py

---
+++ madLibs.py
@@ -0,0 +1,21 @@
+# Goal
+#
+# Create a Mad Libs style game, where the program asks the user for certain types of words, and then prints out a story with the words that the user inputted. The story doesn't have to be too long, but it should have some sort of story line.
+
+noun_one = str(input("Noun? "))
+noun_three = str(input("Noun? "))
+verb_two = str(input("Verb? "))
+noun_two = str(input("Noun? "))
+adjective_one = str(input("Adjective? "))
+noun_four = str(input("Noun? "))
+noun_six = str(input("Noun? "))
+verb_one = str(input("Verb? "))
+noun_five = str(input("Noun? "))
+adjective_two = str(input("Adjective? "))
+
+print("It was Halloween and I was ready to trick or treat.  I was going as a " + noun_one +
+      ". My friend " + noun_two + " was going to be a " + noun_three + ". My friend is so " + adjective_one +
+      ". We rang the bell and a " + noun_four + " answered. We weren't expecting that.  We took our " + noun_five +
+      " and ran! We turned the corner and saw a huge " + noun_six + ". I started " + verb_one + " and my friend started " + verb_two +
+      " because that's all we could think to do.  It was the most " + adjective_two + " Halloween of our lives!")
+
